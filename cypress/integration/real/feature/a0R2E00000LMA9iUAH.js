df016902e71765f058f01cef3fb0a02cb79233eb
Alex Finnarn <alexfinnarn@gmail.com>
2020-07-15 10:25:13 -0400

    try test run for realz

--- cypress/integration/real/feature/a0R2E00000LMA9iUAH.js
+++ cypress/integration/real/feature/a0R2E00000LMA9iUAH.js
@@ -1,7 +1,7 @@
 describe("DE Order Tests", () => {
   beforeEach(() => {
     cy.exec(
-      `sfdx force:org:display -u "feature/a0R2E00000LMA9iUAH" --json | sed -r "s/[[:cntrl:]][[0-9]{1,3}m//g"`
+      `sfdx force:org:display -u $TRAVIS_PULL_REQUEST_BRANCH --json | sed -r "s/[[:cntrl:]][[0-9]{1,3}m//g"`
     ).then((response) => {
       let result = JSON.parse(response.stdout).result;
       let sessionId = result.accessToken;
